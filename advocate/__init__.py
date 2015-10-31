4cb97e75fd1438798ec1a6e59b64b4d49f2cd3bb
Jordan <jordan.milne@saynotolinux.com>
2015-10-31 02:39:44 -0700

    Import everything from requests' __init__.py into ours

--- advocate/__init__.py
+++ advocate/__init__.py
@@ -1,5 +1,13 @@
 __version__ = "0.3.0"
 
+from requests import utils
+from requests.models import Request, Response, PreparedRequest
+from requests.status_codes import codes
+from requests.exceptions import (
+    RequestException, Timeout, URLRequired,
+    TooManyRedirects, HTTPError, ConnectionError
+)
+
 from .adapters import BlacklistingHTTPAdapter
 from .api import (
     request,
